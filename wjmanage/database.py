45dcf54904c9c0f801404891785d5201a1d37b5d
Joe Janicki <j.janicki@oteraaudio.com>
2026-07-08 10:52:59 +0200

    Getall and rearrange database functions

--- wjmanage/database.py
+++ wjmanage/database.py
@@ -62,12 +62,14 @@ class InventoryItem:
     Parameters:
         index (int): The ID/index of the item
         name (str): The name of the item
+        category (str): The item's category
         quantity (str): How many of the item exist
         unit_value (money.Money): How much one of the item cost when purchased
     '''
 
     index: int
     name: str
+    category: str
     quantity: int
     unit_value: money.Money
 
@@ -80,12 +82,14 @@ class FinanceAccount:
     Parameters:
         index (int): The ID/index of the account
         name (str): The name of the account
+        category (str): The account's category
         currency (str): The currency of all the account's transactions
         balance (money.Money): The current calculated balance of the account
     '''
 
     index: int
     name: str
+    category: str
     currency: str
     balace: money.Money
 
@@ -97,6 +101,7 @@ class Transaction:
     Parameters:
         index (int): The ID/index of the transaction
         account (FinanceAccount): The account that the transaction took place in
+        category (str): The transaction's category
         item (InventoryItem|None): The item that this transaction affects
         quantity (int): The quantity that the transaction contributes to the inventory
         description (str): The description of the transaction
@@ -106,6 +111,7 @@ class Transaction:
 
     index: int
     account: FinanceAccount
+    category: str
     item: InventoryItem|None
     quantity: int
     description: str
@@ -135,146 +141,44 @@ class DatabaseConnection:
         self._cur = self._con.cursor()
         self._init_tables()
 
-    def _init_tables(self) -> None:
+    def __del__(self) -> None:
         '''
-        Initialize empty sqlite tables if none exist
+        Automatically close the connection to the database.
+        Nothing happens if it is already closed.
 
         Returns:
             None
         '''
 
-        table_defs = {
-            "transactions": ["id", "account_id", "item_id", "quantity", "description", "amount", "year", "month", "day"],
-            "accounts": ["id", "name", "currency", "balance"],
-            "inventory": ["id", "name", "quantity", "currency", "unit_value"],
-            "employees": ["id", "username", "name", "email", "position", "salt", "password_hash", "permissions", "year", "month", "day"]
-        }
-        for table_name in table_defs:
-            self._cur.execute(
-                f"CREATE TABLE IF NOT EXISTS {table_name}({', '.join(table_defs[table_name])})"
-            )
-        self._con.commit()
+        self.close()
 
-
-    def _table_insert(self, table_name: str, values: list) -> None:
+    def get_all_items(self) -> list[InventoryItem]:
         '''
-        Inserts a list of values into an sqlite3 table of a given name
-        The table must already exist.
+        Gets a list of all InventoryItem objecst in the database
 
-        Parameters:
-            table_name (str): The table to insert into
-            values (list): A list of values that correspond to the table's columns
         Returns:
-            None
+            list[InventoryItem]: A list of all inventory items
         '''
+        return [self._list_to_item(item) for item in self._table_getall('inventory')]
 
-        self._cur.execute(
-            f"INSERT INTO {table_name} VALUES({', '.join([str(i) for i in values])})"
-        )
-        self._con.commit()
 
-    def _table_get(self, table_name: str, key: str, value: str):
+    def get_all_transactions(self) -> list[Transacion]:
         '''
-        Gets a single row from a table where the key and value pair match.
+        Gets a list of all Transacion objects in the database
 
-        Parameters:
-            table_name (str): The table to search
-            key (str): The column name to search by
-            value (str): The value to compare
         Returns:
-            list: list of all returned columns, empty if none found
+            list[Transacion]: A list of all transactions
         '''
-        res = self._cur.execute(f"SELECT * FROM {table_name} WHERE {key}={value}")
-        return res.fetchone()
+        return [self._list_to_transaction(transaction) for transaction in self._table_getall('transactions')]
 
-    def _table_getall(self, table_name: str):
+    def get_all_accounts(self) -> list[FinanceAccount]:
         '''
-        Gets a list of all rows in a table.
+        Gets a list of all FinanceAccount objects in the database
 
-        Parameters:
-            table_name (str): The name of the table
         Returns:
-            list: a 2D list of all of the table's rows
+            list[FinanceAccount]: A list of all accounts
         '''
-        res = self._cur.execute(f"SELECT * FROM {table_name}")
-        return res.fetchall()
-
-    def _list_to_transaction(self, response_list: list) -> Transaction:
-        '''
-        Converts a raw database list to a Transaction object.
-
-        Parameters:
-            response_list (list): The list of raw data from the database response
-        Returns:
-            Transaction: The converted transaction object
-        '''
-        account = self.get_account_by_id(response_list[1])
-        return Transaction(
-            response_list[0],
-            account,
-            self.get_item_by_id(response_list[2]),
-            response_list[3],
-            response_list[4],
-            money.Money(response_list[5], account.currency),
-            date.Date(response_list[6], response_list[7], response_list[8])
-        )
-
-    def _transaction_to_list(self, transaction: Transaction) -> list:
-        '''
-        Converts a Transaction object into a list of raw data for the database.
-
-        Parameters:
-            transaction (Transaction): The transaction to convert
-        Returns:
-            list - The list of raw values
-        '''
-        return [transaction.index, transaction.account.index, transaction.item.index,
-                transaction.quantity, f"'{transaction.description}'", 
-                transaction.amount.get_value_cents(), transaction.date.year, 
-                transaction.date.month, transaction.date.day]
-
-    def _account_to_list(self, account: FinanceAccount) -> list:
-        '''
-        Converts a FinanceAccount object into a list of raw data for the database.
-
-        Parameters:
-            account (FinanceAccount): The transaction to convert
-        Returns:
-            list - The list of raw values
-        '''
-        return [account.index, f"'{account.name}'", f"'{account.currency}'", account.balance.get_value_cents()]
-
-    def _list_to_account(self, response_list: list) -> FinanceAccount:
-        '''
-        Converts a raw database list to a FinanceAccount object.
-
-        Parameters:
-            response_list (list): The list of raw data from the database response
-        Returns:
-            FinanceAccount: The converted account object
-        '''
-        return FinanceAccount(
-            response_list[0],
-            response_list[1],
-            response_list[2],
-            money.Money(response_list[3], response_list[2])
-        )
-
-    def _list_to_item(self, response_list: list) -> InventoryItem:
-        '''
-        Converts a raw database list to a FinanceAccount object.
-
-        Parameters:
-            response_list (list): The list of raw data from the database response
-        Returns:
-            FinanceAccount: The converted account object
-        '''
-        return InventoryItem(
-            response_list[0],
-            response_list[1],
-            response_list[2],
-            money.Money(response_list[3],response_list[4])
-        )
+        return [self._list_to_account(transaction) for account in self._table_getall('accounts')]
 
     def get_account_by_id(self, index: int) -> FinanceAccount|None:
         '''
@@ -334,13 +238,170 @@ class DatabaseConnection:
             self._con.close()
         self._con = None
 
-    def __del__(self) -> None:
+    def _init_tables(self) -> None:
         '''
-        Automatically close the connection to the database.
-        Nothing happens if it is already closed.
+        Initialize empty sqlite tables if none exist
 
         Returns:
             None
         '''
 
-        self.close()
+        table_defs = {
+            "transactions": ["id", "account_id", "category", "item_id", "quantity", "description", "amount", "year", "month", "day"],
+            "accounts": ["id", "name", "category", "currency", "balance"],
+            "inventory": ["id", "name", "category", "quantity", "currency", "unit_value"],
+            "employees": ["id", "username", "name", "email", "position", "salt", "password_hash", "permissions", "year", "month", "day"]
+        }
+        for table_name in table_defs:
+            self._cur.execute(
+                f"CREATE TABLE IF NOT EXISTS {table_name}({', '.join(table_defs[table_name])})"
+            )
+        self._con.commit()
+
+    def _table_insert(self, table_name: str, values: list) -> None:
+        '''
+        Inserts a list of values into an sqlite3 table of a given name
+        The table must already exist.
+
+        Parameters:
+            table_name (str): The table to insert into
+            values (list): A list of values that correspond to the table's columns
+        Returns:
+            None
+        '''
+
+        self._cur.execute(
+            f"INSERT INTO {table_name} VALUES({', '.join([str(i) for i in values])})"
+        )
+        self._con.commit()
+
+    def _table_get(self, table_name: str, key: str, value: str):
+        '''
+        Gets a single row from a table where the key and value pair match.
+
+        Parameters:
+            table_name (str): The table to search
+            key (str): The column name to search by
+            value (str): The value to compare
+        Returns:
+            list: list of all returned columns, empty if none found
+        '''
+        res = self._cur.execute(f"SELECT * FROM {table_name} WHERE {key}={value}")
+        return res.fetchone()
+
+    def _table_getall(self, table_name: str):
+        '''
+        Gets a list of all rows in a table.
+
+        Parameters:
+            table_name (str): The name of the table
+        Returns:
+            list: a 2D list of all of the table's rows
+        '''
+        res = self._cur.execute(f"SELECT * FROM {table_name}")
+        return res.fetchall()
+
+
+    def _list_to_transaction(self, response_list: list) -> Transaction:
+        '''
+        Converts a raw database list to a Transaction object.
+
+        Parameters:
+            response_list (list): The list of raw data from the database response
+        Returns:
+            Transaction: The converted transaction object
+        '''
+        account = self.get_account_by_id(response_list[1])
+        return Transaction(
+            response_list[0],
+            account,
+            response_list[2],
+            self.get_item_by_id(response_list[3]),
+            response_list[4],
+            response_list[5],
+            money.Money(response_list[6], account.currency),
+            date.Date(response_list[7], response_list[8], response_list[9])
+        )
+
+    def _transaction_to_list(self, transaction: Transaction) -> list:
+        '''
+        Converts a Transaction object into a list of raw data for the database.
+
+        Parameters:
+            transaction (Transaction): The transaction to convert
+        Returns:
+            list - The list of raw values
+        '''
+        return [transaction.index, transaction.account.index, f"'{transaction.category}'", 
+                transaction.item.index,
+                transaction.quantity, f"'{transaction.description}'", 
+                transaction.amount.get_value_cents(), transaction.date.year, 
+                transaction.date.month, transaction.date.day]
+
+
+    def _account_to_list(self, account: FinanceAccount) -> list:
+        '''
+        Converts a FinanceAccount object into a list of raw data for the database.
+
+        Parameters:
+            account (FinanceAccount): The transaction to convert
+        Returns:
+            list - The list of raw values
+        '''
+        return [account.index, f"'{account.name}'", 
+                f"'{account.category}'"
+                f"'{account.currency}'", 
+                account.balance.get_value_cents()]
+
+    def _item_to_list(self, item: InventoryItem) -> list:
+        '''
+        Converts an InventoryItem object into a list of raw data for the database.
+
+        Parameters:
+            item (InventoryItem): The item to convert
+        Returns:
+            list - The list of raw values
+        '''
+        return [
+            item.index,
+            f"'{item.name}'",
+            f"'{item.category}'",
+            item.quantity,
+            f"'{item.unit_value.get_currency()}'",
+            item.unit_value.get_value_cents()
+        ]
+
+    def _list_to_account(self, response_list: list) -> FinanceAccount:
+        '''
+        Converts a raw database list to a FinanceAccount object.
+
+        Parameters:
+            response_list (list): The list of raw data from the database response
+        Returns:
+            FinanceAccount: The converted account object
+        '''
+        return FinanceAccount(
+            response_list[0],
+            response_list[1],
+            response_list[2],
+            response_list[3],
+            money.Money(response_list[4], response_list[3])
+        )
+
+    def _list_to_item(self, response_list: list) -> InventoryItem:
+        '''
+        Converts a raw database list to a FinanceAccount object.
+
+        Parameters:
+            response_list (list): The list of raw data from the database response
+        Returns:
+            FinanceAccount: The converted account object
+        '''
+        return InventoryItem(
+            response_list[0],
+            response_list[1],
+            response_list[2],
+            response_list[3],
+            money.Money(response_list[5],response_list[4])
+        )
+
